5408fc2a464cd94ef5611c4ee680eadcaef3a8d9
Joost Flick <joostflick@gmail.com>
2018-11-01 13:23:49 +0100

    Added hacky fix to make multiple requests

--- index.js
+++ index.js
@@ -19,41 +19,53 @@ const client = new OBA({
 
 // Client returns a promise which resolves the APIs output in JSON
 
-// Example search to the word 'rijk' sorted by title:
-client.get('search', {
-  q: 'test',
-  sort: 'title',
-  facet: 'type(book)',
-  refine: true
-  //page: 1
-})
-.then(function(res){
-  //fs.writeFile('myjsonfile.json', res, 'utf8', function(){})
-  const boeken = [];
-  JSON.parse(res).aquabrowser.results.result.forEach(function(boek, id) {
+const boeken = [];
+const amountPages = 5;
+var pageIndex = 0;
 
-    // boek.titles.title.$t// titel
-    // boek.authors['main-author'].$t // hoofdauteur
-    // boek.authors.author.$t // auteur
-    // boek.languages.language.$t // taal
-    // boek.publication.year.$t // jaartal
-    // boek.summaries.summary.$t // samenvatting
-
-    var boekInstantie = {
-      id : id,
-      titel : boek.titles.title.$t,
-      taal : (typeof boek.languages === "undefined") ? 'Taal onbekend' : boek.languages.language.$t,
-      jaartal : (typeof boek.publication === "undefined" || typeof boek.publication.year === "undefined") ? 'Jaar onbekend' : boek.publication.year.$t,
-      author : (typeof boek.authors === "undefined" || typeof boek.authors['main-author'] === "undefined")  ? "Auteur onbekend" : boek.authors['main-author'].$t
-    }
-    
-    boeken.push(boekInstantie)
-    //boeken.push(["id: " + id, "titel: " + boek.titles.title.$t, "taal: " + boek.languages.language.$t]);
-
-    // boeken.map(function (value, index) {
-    //   return index;
-    // })
+for(i=0; i<amountPages; i++){
+  pageIndex++
+  client.get('search', {
+    q: 'test',
+    sort: 'title',
+    facet: 'type(book)',
+    refine: true,
+    page: pageIndex
   })
+  .then(function(res){
+    fs.writeFile('myjsonfile.json', res, 'utf8', function(){})
+    JSON.parse(res).aquabrowser.results.result.forEach(function(boek, id) {
+      
+      // boek.id.nativeid //id
+      // boek.titles.title.$t// titel
+      // boek.authors['main-author'].$t // hoofdauteur
+      // boek.authors.author.$t // auteur
+      // boek.languages.language.$t // taal
+      // boek.publication.year.$t // jaartal
+      // boek.summaries.summary.$t // samenvatting
+  
+      var boekInstantie = {
+        pageId : id,
+        id : boek.id.nativeid,
+        titel : boek.titles.title.$t,
+        taal : (typeof boek.languages === "undefined") ? 'Taal onbekend' : boek.languages.language.$t,
+        jaartal : (typeof boek.publication === "undefined" || typeof boek.publication.year === "undefined") ? 'Jaar onbekend' : boek.publication.year.$t,
+        author : (typeof boek.authors === "undefined" || typeof boek.authors['main-author'] === "undefined")  ? "Auteur onbekend" : boek.authors['main-author'].$t
+      }
+      
+      boeken.push([boekInstantie.id, boekInstantie.jaartal, boekInstantie.taal])
+      //boeken.push(["id: " + id, "titel: " + boek.titles.title.$t, "taal: " + boek.languages.language.$t]);
+  
+      // boeken.map(function (value, index) {
+      //   return index;
+      // })
+    })
+    //console.log(boeken)
+  })
+    .catch(err => console.log(err)) // Something went wrong in the request to the API
+
+}
+setTimeout(function(){ 
   console.log(boeken)
-})
-  .catch(err => console.log(err)) // Something went wrong in the request to the API
+  //fs.writeFile('dataset.js', boeken, 'utf8', function(){})
+}, 4000);

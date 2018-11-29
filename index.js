55e2e34a9a6f1410f81ddae4efe49acf35f0fe42
Joost Flick <joostflick@gmail.com>
2018-11-29 12:09:44 +0100

    Linting

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const OBA = require('oba-api');
-var fs = require('fs');
+const OBA = require('oba-api')
+var fs = require('fs')
 require('dotenv').config()
 
 // Take keys from .env
-var public_key = process.env.DB_PUBLIC;
-var secret_key = process.env.DB_SECRET;
+var public_key = process.env.DB_PUBLIC
+var secret_key = process.env.DB_SECRET
 
 // Set up authentication to api server
 const client = new OBA({
   public: public_key,
   secret: secret_key
-});
+})
 
 // Empty arrays
 const boeken = []
 const percentageArray = []
-
 
 // Empty promises array
 promises = []
@@ -24,36 +23,47 @@ promises = []
 var amountPages = 5
 
 // Which year
-var year = 2018
+var year = 2000
 
 // Search query
 var query = 'year:' + year + ' test'
 
 // Get results for each page
-for (var i = 0; i < amountPages; i++) getResults((i + 1), query);
+for (var i = 0; i < amountPages; i++) getResults(i + 1, query)
 
 // Get results with page and query parameters
 function getResults(page, query) {
   // Make a promise for each request
   promises.push(
     // Get request
-    client.get('search', {
-      q: query,
-      //sort: 'title',
-      facet: 'type(book)',
-      refine: true,
-      page: page
-    })
-      .then(function (res) {
-        JSON.parse(res).aquabrowser.results.result.forEach(function (boek, id) {
-
+    client
+      .get('search', {
+        q: query,
+        //sort: 'title',
+        facet: 'type(book)',
+        refine: true,
+        page: page
+      })
+      .then(function(res) {
+        JSON.parse(res).aquabrowser.results.result.forEach(function(boek, id) {
           var boekInstantie = {
             //pageId : id,
             id: parseInt(boek.id.nativeid),
             titel: boek.titles.title.$t,
-            taal: (typeof boek.languages === "undefined") ? 'Taal onbekend' : boek.languages.language.$t,
-            jaartal: (typeof boek.publication === "undefined" || typeof boek.publication.year === "undefined") ? 'Jaar onbekend' : parseInt(boek.publication.year.$t),
-            author: (typeof boek.authors === "undefined" || typeof boek.authors['main-author'] === "undefined") ? "Auteur onbekend" : boek.authors['main-author'].$t
+            taal:
+              typeof boek.languages === 'undefined'
+                ? 'Taal onbekend'
+                : boek.languages.language.$t,
+            jaartal:
+              typeof boek.publication === 'undefined' ||
+              typeof boek.publication.year === 'undefined'
+                ? 'Jaar onbekend'
+                : parseInt(boek.publication.year.$t),
+            author:
+              typeof boek.authors === 'undefined' ||
+              typeof boek.authors['main-author'] === 'undefined'
+                ? 'Auteur onbekend'
+                : boek.authors['main-author'].$t
           }
           // Push all book objects to the boeken array
           boeken.push(boekInstantie)
@@ -67,12 +77,13 @@ function getResults(page, query) {
 // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
 function dynamicSort(property) {
   var sortOrder = 1
-  if (property[0] === "-") {
+  if (property[0] === '-') {
     sortOrder = -1
     property = property.substr(1)
   }
-  return function (a, b) {
-    var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0
+  return function(a, b) {
+    var result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0
     return result * sortOrder
   }
 }
@@ -81,33 +92,34 @@ function dynamicSort(property) {
 // This removes duplicates based on a certain property
 // https://stackoverflow.com/questions/2218999/remove-duplicates-from-an-array-of-objects-in-javascript
 function removeDuplicate(originalArray, prop) {
-  var newArray = [];
-  var lookupObject = {};
+  var newArray = []
+  var lookupObject = {}
 
   for (var i in originalArray) {
-    lookupObject[originalArray[i][prop]] = originalArray[i];
+    lookupObject[originalArray[i][prop]] = originalArray[i]
   }
 
   for (i in lookupObject) {
-    newArray.push(lookupObject[i]);
+    newArray.push(lookupObject[i])
   }
-  return newArray;
+  return newArray
 }
 
 // If all promises are resolved, do this
-Promise.all(promises).then(function (res) {
+Promise.all(promises).then(function(res) {
   //console.log(boeken)
 
   // Remove duplicates and sort by year
-  var cleanedBoeken = removeDuplicate(boeken.sort(dynamicSort("jaartal")), "id");
+  var cleanedBoeken = removeDuplicate(boeken.sort(dynamicSort('jaartal')), 'id')
 
   // Empty array for different languages
   languages = []
 
   var differentLanguages = removeDuplicate(cleanedBoeken, 'taal')
 
-  differentLanguages.forEach(function (boek) {
+  differentLanguages.forEach(function(boek) {
     languages.push(boek.taal)
+    console.log(languages)
   })
 
   // Code below needs to be updated to be dynamic
@@ -116,7 +128,7 @@ Promise.all(promises).then(function (res) {
   boekenGer = []
   var boekenOther = 0
 
-  cleanedBoeken.forEach(function (boek) {
+  cleanedBoeken.forEach(function(boek) {
     if (boek.taal === languages[0]) {
       boekenDut.push(boek)
     } else if (boek.taal === languages[1]) {
@@ -129,28 +141,52 @@ Promise.all(promises).then(function (res) {
   })
   //console.log(boekenDut)
 
-  console.log("Aantal boeken uit " + year + " in de taal Nederlands: " + boekenDut.length)
+  console.log(
+    'Aantal boeken uit ' + year + ' in de taal Nederlands: ' + boekenDut.length
+  )
   //console.log(boekenEng)
-  console.log("Aantal boeken uit " + year + " in de taal Engels: " + boekenEng.length)
-  console.log("Aantal boeken uit " + year + " in de taal Duits: " + boekenGer.length)
+  console.log(
+    'Aantal boeken uit ' + year + ' in de taal Engels: ' + boekenEng.length
+  )
+  console.log(
+    'Aantal boeken uit ' + year + ' in de taal Duits: ' + boekenGer.length
+  )
 
-  fs.writeFile('myjsonfile.json', JSON.stringify(cleanedBoeken, null, '  '), 'utf8', function () { })
+  fs.writeFile(
+    'myjsonfile.json',
+    JSON.stringify(cleanedBoeken, null, '  '),
+    'utf8',
+    function() {}
+  )
 
-
-  var percentages = calculatePercentage(boekenDut.length, boekenEng.length, boekenGer.length, boekenOther);
-  percentageArray.push({ year: year, lang: 'Dutch', value: percentages[0] }, { year: year, lang: 'English', value: percentages[1] }, { year: year, lang: 'German', value: percentages[2] }, { year: year, lang: 'Other', value: percentages[3] })
+  var percentages = calculatePercentage(
+    boekenDut.length,
+    boekenEng.length,
+    boekenGer.length,
+    boekenOther
+  )
+  percentageArray.push(
+    { year: year, lang: 'Dutch', value: percentages[0] },
+    { year: year, lang: 'English', value: percentages[1] },
+    { year: year, lang: 'German', value: percentages[2] },
+    { year: year, lang: 'Other', value: percentages[3] }
+  )
   console.log(percentageArray)
-  fs.writeFile('d3test/percentages.json', JSON.stringify(percentageArray, null, '  '), 'utf8', function () { })
-
+  fs.writeFile(
+    'docs/percentages.json',
+    JSON.stringify(percentageArray, null, '  '),
+    'utf8',
+    function() {}
+  )
 })
 
 // Calculate percentages
 function calculatePercentage(dut, eng, ger, other) {
   var total = dut + eng + ger + other
-  var percentDut = dut * 100 / total;
-  var percentEng = eng * 100 / total;
-  var percentOther = other * 100 /total;
-  var percentGer = ger * 100 / total;
+  var percentDut = (dut * 100) / total
+  var percentEng = (eng * 100) / total
+  var percentOther = (other * 100) / total
+  var percentGer = (ger * 100) / total
 
   return [percentDut, percentEng, percentGer, percentOther]
 }
